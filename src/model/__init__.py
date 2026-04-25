14a182ccd5459758638e1022ef2cf54a023a93ce
Eender753030 <ender303030@gmail.com>
2026-04-26 01:54:23 +0800

    refactor: Change model/ path and make more custom setting for model

---
+++ src/model/__init__.py
@@ -0,0 +1,10 @@
+from .model import SimpleCNN
+
+def get_model(config: dict) -> SimpleCNN:
+    model_config = config["model"]
+    return SimpleCNN (
+        num_class=model_config["num_class"],
+        num_stage=model_config["num_stage"],
+        num_res=model_config["num_res"],
+        dropout=model_config["dropout"],
+    )
