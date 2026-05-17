abba4dbd606cdd28038e2ecc398251beb80c0332
Eender753030 <ender303030@gmail.com>
2026-05-17 12:13:26 +0800

    chore: Update model configures

--- emotion_detect/src/model/__init__.py
+++ emotion_detect/src/model/__init__.py
@@ -7,4 +7,5 @@ def get_model(config: dict) -> SimpleCNN:
         num_stage=model_config["num_stage"],
         num_res=model_config["num_res"],
         dropout=model_config["dropout"],
+        base_channels=model_config.get("base_channels", 32)
     )
